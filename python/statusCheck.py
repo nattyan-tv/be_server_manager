062a6c5a464babd47c9ce1bf6e70272f8740a453
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2022-04-25 08:40:19 +0900

    add server status checker(beta)

---
+++ python/statusCheck.py
@@ -0,0 +1,18 @@
+import os, sys, json, asyncio, time
+import mcstatus
+
+# Check Minecraft server status and show it.
+
+def cls():
+    os.system("cls")
+
+def main(address: str, port: int):
+    while True:
+        print(f"""\
+Minecraft Server {address}:{port}""")
+
+    return
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], int(sys.argv[2]))
