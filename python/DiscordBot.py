10fc2d3134cb0a54af6cea5bd5ceba7dd6f0db65
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2022-04-26 08:19:37 +0900

    add bot

--- python/DiscordBot.py
+++ python/DiscordBot.py
@@ -3,4 +3,19 @@ from mcstatus import BedrockStatus as mcb
 import requests, webhook
 import discord
 
-bot = 
+intents = nextcord.Intents.all()
+intents.typing = False
+intents.presences = True
+intents.members = True
+bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
+bot.remove_command("help")
+
+
+
+@bot.event
+async def on_ready():
+    print("DiscordBot Launched...")
+
+@bot.command()
+async def info(ctx: discord.command.Context):
+    print(ctx.message.author)
